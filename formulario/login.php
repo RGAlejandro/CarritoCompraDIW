0f3c8d846e1af2e0b55e0433895a40b1634f03f4
PM-PRG <130345386+PM-PRG@users.noreply.github.com>
2024-02-22 19:53:15 +0100

    login.php logica php
    se me olvido subirlo jeje

---
+++ formulario/login.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "carrito";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $username = $_POST["username"]; // es el email, no el usuario realmente
+    $password = $_POST["password"];
+
+    
+    $authenticated = false; 
+
+    $sql = "SELECT * FROM usuarios WHERE Usuario_email='$username' AND Usuario_clave='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) { // si hay filas, la combinacion de email y password son correctas
+        $row = $result->fetch_assoc();
+
+        // almacenar los datos del usuario en variables de sesión
+        $_SESSION['id_usuario'] = $row['id_usuario'];
+        $_SESSION['Usuario_email'] = $row['Usuario_email'];
+        $_SESSION['nombre'] = $row['nombre'];
+        $_SESSION['apellidos'] = $row['apellidos'];
+        $_SESSION['telefono'] = $row['telefono'];
+        $_SESSION['perfil'] = $row['perfil'];
+
+        $authenticated = true;
+        
+    } else { // si no, la combinacion es incorrecta
+        
+        $authenticated = false;
+    }
+
+    if ($authenticated) {
+       
+        header("Location: index.php?login=1");
+        exit();
+    } else {
+        header("Location: index.php?login=0");
+        exit();
+    }
+}
+?>
